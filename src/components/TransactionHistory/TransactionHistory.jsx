121a18d1f4a843939cca04221aef8d4d303cc013
Your Name <you@example.com>
2024-08-10 14:02:10 +0300

    fix problems

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -4,11 +4,13 @@ import TransactionHistoryRow from '../TransactionHistoryRow/TransactionHistoryRo
 export default function TransactionHistory({ operations }) {
   return (
     <table className={css.tableHistory}>
-      <tr>
-        <th className={css.tableHeader}>Type</th>
-        <th className={css.tableHeader}>Amount</th>
-        <th className={css.tableHeader}>Currency</th>
-      </tr>
+      <thead>
+        <tr>
+          <th className={css.tableHeader}>Type</th>
+          <th className={css.tableHeader}>Amount</th>
+          <th className={css.tableHeader}>Currency</th>
+        </tr>
+      </thead>
       <tbody>
         {operations.map(operation => (
           <tr className={css.tableRow} key={operation.id}>
